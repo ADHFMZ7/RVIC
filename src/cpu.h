07022d1506f28d1f0caa2d0dab4cd280bc888c03
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-01-11 10:54:15 -0800

    create cpu struct

---
+++ src/cpu.h
@@ -0,0 +1,16 @@
+#ifndef CPU_H
+#define CPU_H
+
+#include "macros.h"
+
+typedef struct core
+{
+	// registers
+	u32 pc;
+	u32 *x;
+	
+	// memory
+	
+} core;
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "macros.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include "cpu.h"
 
 #define address_space 1024*1024*1024*4
 
@@ -8,16 +10,22 @@ i32 main(int argc, char **argv)
 
 	// initialize memory space	
 
+	core cpu;
+
 	u8 *memory = (u8 *) malloc (sizeof(u8) * 1024);
+	memory[0] = 0xFF;
+	memory[1] = 0x00;
+	memory[2] = 0xF1;
+	memory[3] = 0x13;
+
 
 	// initialize registers
-	u32 pc;
+	cpu.pc = 0;
 
-	u32 *x = (u32 *) malloc(sizeof(u32) * 32);
-	x[0] = 0;
+	cpu.x = (u32 *) malloc(sizeof(u32) * 32);
+	cpu.x[0] = 0;
 
 	// load program into memory
-
 
 	// start fetch/execute/decode cycle
 
@@ -27,9 +35,22 @@ i32 main(int argc, char **argv)
 	{
 		// fetch
 
+
+		// 0x FF 00 F1 13
+
+		u32 instruction = 0;
+
+		for (int i = 0; i < 4; i++) instruction += memory[cpu.pc++] << (8 * (3-i));
+		
+		u8 opcode = 0x7F & instruction;
+
 		// decode
 
+
 		// execute
+
+
+		running = 0;
 	}
 
 }
